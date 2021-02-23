31fd0ef64af28b575789e2fc4f5ea73f87e90a65
Muzhirul Islam <muzhirulislam@gmail.com>
2021-02-23 09:28:27 +0600

    create StockBalanceRepository

---
+++ src/main/java/com/example/invmgnt/invmgnt/repository/StockBalanceRepository.java
@@ -0,0 +1,9 @@
+package com.example.invmgnt.invmgnt.repository;
+
+import com.example.invmgnt.invmgnt.domain.StockBalance;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StockBalanceRepository extends JpaRepository<StockBalance, Long> {
+}
